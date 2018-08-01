7c772191822b3db2aa944390c4666d0dfbf1a547
Machoman <rbunnicelli2010@yahoo.com>
2018-08-01 19:07:56 -0400

    app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD1-vEq86gWS_GMuvv7-2Iiuz6luUkm-UU",
+    authDomain: "trainscheduler-245bb.firebaseapp.com",
+    databaseURL: "https://trainscheduler-245bb.firebaseio.com",
+    projectId: "trainscheduler-245bb",
+    storageBucket: "trainscheduler-245bb.appspot.com",
+    messagingSenderId: "512040376400"
+  };
+  firebase.initializeApp(config);
+</script>
